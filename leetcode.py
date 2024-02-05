d4f0ddf9d0489e5273c3eda249ced8652e7e6e39
Mohammad Ahrazul Haque <117664890+Ahrazul@users.noreply.github.com>
2024-02-04 22:07:38 -0500

    Update leetcode.py

--- leetcode.py
+++ leetcode.py
@@ -1,1 +1,20 @@
+# Date: 2/4/2024
+# Problem - Add Two Numbers (Medium)
+# Solution inspired from NeetCode
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        dummy = ListNode()
+        curr = dummy
+        carry = 0
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+            val = val1 + val2 + carry
+            carry = val // 10
+            val = val % 10
+            curr.next = ListNode(val)
+            curr = curr.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+        return dummy.next
 
