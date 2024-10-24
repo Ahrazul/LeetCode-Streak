c9a3c68aa5b8c6dbd9b01b30c4512c46afce954a
Mohammad Ahrazul Haque <117664890+Ahrazul@users.noreply.github.com>
2024-10-24 14:39:05 -0400

    Create Unique-number-of-occurences.py

---
+++ Unique-number-of-occurences.py
@@ -0,0 +1,21 @@
+# Problem: Unique number of occurences
+# Difficulty: Medium
+# Runtime: 4 ms (Beats 94% of LeetCode users)
+
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        ref = {}
+
+        for i in arr:
+            if i in ref.keys():
+                ref[i] += 1
+            else:
+                ref[i] = 1
+        
+        lst = list(ref.values())
+
+        if len(lst) == len(set(lst)):
+            return True
+        else:
+            return False
+        
